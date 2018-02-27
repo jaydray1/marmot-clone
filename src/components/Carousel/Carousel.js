78587c2afe4fd44949f94611347e232100fa8bf8
JM <jdreher15@gmail.com>
2018-02-26 23:17:35 -0700

    carousel styling

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -6,7 +6,7 @@ import '../Carousel/Carousel.css';
 export default class Carousel extends Component {
     render() {
         var settings = {
-            dots: true,
+            dots: false,
             infinite: true,
             speed: 500,
             slidesToShow: 4,
@@ -16,9 +16,9 @@ export default class Carousel extends Component {
         };
         var Jackets = jackets.map(function(jacket) {
             return <div className="slide-deck" key={jacket.id}>
-                        <img src={jacket.image} alt="jacket" width="227" height="407"/>
-                            <p>{jacket.title}</p>
-                            <p>${jacket.price}</p>
+                        <img src={jacket.image} alt="jacket" width="227" height="407" className="img"/>
+                            <div className="jacket-title">{jacket.title}</div>
+                            <div className="jacket-price">${jacket.price}</div>
                    </div>
         })
         return (
