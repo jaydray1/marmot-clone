b17e551a3faab1f279f608df40628e351d2cb8d3
JM <jdreher15@gmail.com>
2020-01-04 06:53:15 -0800

    update stylesz.

--- src/components/Adventures/Adventures.js
+++ src/components/Adventures/Adventures.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import storyBanner from '../../assets/marmotDeskStoryBanner.jpeg';
 
 import '../Adventures/Adventures.css';
 
-export default function Adventures (props) {
+export default function Adventures () {
         return(
             <div className="tiles-wrapper">
-                    <img src={props.imageName} alt="adventures banner" className="ad-tile"/>
-                    <div className="left-box"></div>
-                    <div className="right-box">
-                    <div className="content-wrapper">
+                    <img src={storyBanner} alt="adventures banner" className="ad-tile"/>
+                    {/* <div className="left-box"></div> */}
+                    {/* <div className="right-box"> */}
+                    {/* <div className="content-wrapper">
                             <p className="category-header">{props.categoryHeader}</p>
                             <h1 className="title-header">{props.titleHeader}</h1>
                             <p className="tile-description">{props.tileDescription}</p>
@@ -16,8 +17,8 @@ export default function Adventures (props) {
                                 <button className="button1"> Read More </button>
                                 <button className="button2">{props.subButton}</button>
                             </div>
-                        </div>
-                    </div>
+                        </div> */}
+                    {/* </div> */}
                         
             </div>
         )
